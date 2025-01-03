059aaa96c2f529f57a70a9abfc97f6227a18f2f3
Abel Rincon <131419488+onebrownguy@users.noreply.github.com>
2025-01-03 15:05:04 -0600

    Time: 0 ms (100%), Space: 22.6 MB (63.88%) - LeetHub

---
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/3236-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <bitset>
+using namespace std;
+
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        int sum = nums[0]; // initialize the sum with first integer of array
+
+        if (nums.size() == 1){
+            if (nums[0] != 0){
+                return nums[0] +1;
+            }
+            else {
+                return 1;
+            }
+
+        }
+
+
+        for (int i = 1; i < nums.size() && nums[i] == nums[i - 1] + 1; ++i){
+                if (nums[i] != nums[i - 1] + 1) {
+                // Non-sequential number found
+                return nums[0] + 1;
+            }
+            sum += nums[i];
+        }
+
+        bitset<51> visited;
+        for (int num : nums){
+            visited[num] = 1;
+        }
+
+        for (int x = sum; x <= 51; ++x){ 
+            if (!visited[x]){           
+                return x;
+            }
+        }
+
+
+        return sum;
+    }
+};
